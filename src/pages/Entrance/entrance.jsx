3e69a2f632a3514abe0342211f23f24fb043378f
Жамал <aidana.nazarova.ch@g,ail.com>
2023-10-21 08:58:51 +0200

    add page login,singup

--- src/pages/Entrance/entrance.jsx
+++ src/pages/Entrance/entrance.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../store/actions';
 import styles from "./entrance.module.scss"
 import { useNavigate } from 'react-router';
-
+import img_2 from '../../images/login.form.png'
 
 export const Entrance = () => {
     const [eye, setEye] = useState(false)
@@ -25,17 +25,29 @@ export const Entrance = () => {
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)} >
+            <div className={styles.form_styles_row}>
+            <div className={styles.form_img}>
+<img className={styles.img} src={img_2} alt='img'/>
+                </div>
+                <div className={styles.form_style}>
                 <input type='username' placeholder='username' {...register('username')} />
+                <div className={styles.flex_row}>
                 <input type={eye ? 'text' : 'password'}
                     placeholder='password'{...register('password')}
                 />
                 {
                     eye ?
-                        (<AiOutlineEye onClick={() => setEye(!eye)} />) :
-                        (<AiOutlineEyeInvisible onClick={() => setEye(!eye)} />)
+                        (<AiOutlineEye className={styles.icon} onClick={() => setEye(!eye)} />) :
+                        (<AiOutlineEyeInvisible className={styles.icon}  onClick={() => setEye(!eye)} />)
                 }
+                      </div>
                 <button type='submite'>Вход</button>
+                </div> 
+
+                </div>
+             
             </form>
+            
         </>
     )
 }
--- src/pages/singup/singup.jsx
+++ src/pages/singup/singup.jsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { postUser } from '../../store/actions';
 import { useNavigate } from 'react-router';
-
+import img from '../../images/singup.form.png'
 export const SingUp = () => {
 
     const [eye, setEye] = useState(false)
@@ -25,20 +25,29 @@ export const SingUp = () => {
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)} >
+                <div className={styles.form_styles_row}>
                 <div className={styles.form_style}>
                     <input type='email' placeholder='email' {...register('email')} />
                     <input type='username' placeholder='username' {...register('username')} />
+                    <div className={styles.flex_row}>
                     <input type={eye ? 'text' : 'password'}
                         placeholder='password'{...register('password')}
 
                     />
                     {
                         eye ?
-                            (<AiOutlineEye onClick={() => setEye(!eye)} />) :
-                            (<AiOutlineEyeInvisible onClick={() => setEye(!eye)} />)
+                            (<AiOutlineEye className={styles.icon} onClick={() => setEye(!eye)} />) :
+                            (<AiOutlineEyeInvisible className={styles.icon}  onClick={() => setEye(!eye)} />)
                     }
+                    </div>
+                  
                     <button type='submite'>Регистрация</button>
                 </div>
+                <div className={styles.form_img}>
+<img className={styles.img} src={img} alt='img'/>
+                </div>
+                </div>
+             
 
             </form>
         </>
